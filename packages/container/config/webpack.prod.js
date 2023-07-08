583773cba067fedee942fa8e121c3d31a2af110c
payalg912517 <payalg912517@gmail.com>
2023-07-08 13:28:24 +0530

    Updated Container Deploy

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domainName = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
@@ -15,7 +15,7 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domainName}/marketing/remoteEntry.js`
             },
             shared: packageJson.dependencies,
         }),
