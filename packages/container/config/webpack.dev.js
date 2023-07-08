1ccbd5e1315e4a4dcf7a7789f5664116e48ec9f2
payalg912517 <payalg912517@gmail.com>
2023-07-08 13:01:46 +0530

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -21,10 +20,7 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
+    ],
 };
 
 module.exports = merge(commonConfig, devConfig);
